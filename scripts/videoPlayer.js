3c1d7af2104c32fc350a0cdcacf2119e04776e54
dmitrybratishkin <40885118+dmitrybratishkin@users.noreply.github.com>
2020-07-30 04:02:28 +0300

    Add files via upload

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -6,7 +6,9 @@ export const videoPlayerInit = () => {
         videoButtonStop = document.querySelector('.video-button__stop'),
         videoProgress = document.querySelector('.video-progress'),
         videoTimePassed = document.querySelector('.video-time__passed'),
-        videoTimeTotal = document.querySelector('.video-time__total');
+        videoTimeTotal = document.querySelector('.video-time__total'),
+        videoVolume = document.querySelector('.video-volume'),
+        videoFullscreen = document.querySelector('.video-fullscreen');
 
 
     const toggleIcon = () => {
@@ -66,5 +68,13 @@ export const videoPlayerInit = () => {
               value = videoProgress.value;
 
         videoPlayer.currentTime = (value * duration) / 100;
+    });
+
+    videoFullscreen.addEventListener('click', () => {
+        videoPlayer.webkitEnterFullScreen();
+    });
+
+    videoVolume.addEventListener('input', () => {
+        videoPlayer.volume = videoVolume.value / 100;
     });
 };
